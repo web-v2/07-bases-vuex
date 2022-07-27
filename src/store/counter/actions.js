ae9f0fef11dde018d9712c04aec08ab0c8b13baa
Samir Vergara V <svergara3@misena.edu.co>
2022-07-26 22:50:28 -0500

    bases vuex

---
+++ src/store/counter/actions.js
@@ -0,0 +1,8 @@
+import getRandomInt from '../../helpers/getRandomInt'
+
+export const incrementRandomInt = async ({ commit }) => {
+    commit('setLoading', true )
+    const randomInt = await getRandomInt()
+    commit('incrementBy', randomInt )
+    commit('setLoading', false )
+}
